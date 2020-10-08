61f070ed58f2e751c99711fbd5d453dbb60a5d55
Filipe Ferreira <filipesferreira94@gmail.com>
2020-10-08 22:50:39 +0100

    union types

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-// const person: {
-//     name: string,
-//     age: number,
-//     hobbies: string[],
-//     role: [number, string] //use a tuple for have exactly this type into this column
-// } = {
-//     name: 'Filipe',
-//     age: 30,
-//     hobbies: ['Sports','Tecnology'],
-//     role: [2, 'author']
-// };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var ADMIN = 0;
-var READ_ONLY = 1;
-var AUTHOR = 2;
-var person = {
-    name: 'Filipe',
-    age: 30,
-    hobbies: ['Sports', 'Tecnology'],
-    role: Role.ADMIN
-};
-// person.role.push('admin');
-//person.role = [5, 'user'];
-//person.role[1] = 10;
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person);
-//From javascript
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    //console.log(hobby.map()); !!! ERROR !!!
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + " " + input2.toString();
+    }
+    return result;
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString() ;
+    // }
 }
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+var combinedAge = combine(30, 26, 'as-number');
+console.log(combinedAge);
+var combinedStringAge = combine('30', '26', 'as-number');
+console.log(combinedStringAge);
+var combinedNames = combine('Max', 'Ana', 'as-text');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,11 +1,28 @@
-console.log('your code goes here!');
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text'){
+    
+    let result;
 
-function add(n1, n2){
-    return n1 + n2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + " " + input2.toString();
+    }
+
+    return result;
+    
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString() ;
+    // }
+
 }
 
-const number1 = 5;
-const number2 = 2.8;
+const combinedAge = combine(30, 26, 'as-number');
+console.log(combinedAge);
 
-const result = add(number1, number2);
-console.log(result);
+const combinedStringAge = combine('30', '26', 'as-number');
+console.log(combinedStringAge);
+
+const combinedNames = combine('Max', 'Ana', 'as-text');
+console.log(combinedNames);
